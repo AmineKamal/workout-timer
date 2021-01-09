195d98ccffa0061fde5930989a680faa974e1f6c
Amine Kamal <m.amine.kamal@gmail.com>
2021-01-09 17:02:09 -0500

    fix missing page

--- src/services/state.ts
+++ src/services/state.ts
@@ -1,6 +1,6 @@
 import { BehaviorSubject } from 'rxjs';
 
-const PAGES = ['home', 'workouts', 'exercices'] as const;
+const PAGES = ['home', 'workouts', 'exercices', 'weight-tracker'] as const;
 export type Page = typeof PAGES[number];
 
 export const State = new class
